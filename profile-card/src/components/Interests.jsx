7ab5ab0f841411d4598ef70212cb41c81603edcf
Martin Yanchev <martin@icecreamarchitecture.com>
2022-09-11 23:04:22 +0100

    setting up the project,adding icons, fonts and colors

---
+++ profile-card/src/components/Interests.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Interests extends Component {
+  render() {
+    return (
+      <div>Interests</div>
+    )
+  }
+}
+
+export default Interests
