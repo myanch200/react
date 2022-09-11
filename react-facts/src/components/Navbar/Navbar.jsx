c7bda2c2ffb5d17a171c8bd92f2906664ed79e8b
Martin Yanchev <martin@icecreamarchitecture.com>
2022-09-11 20:59:36 +0100

    making sure there is only one h1

--- react-facts/src/components/Navbar/Navbar.jsx
+++ react-facts/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <nav className="nav-bar">
       <div className="nav-logo-wrapper">
         <img className="nav-react-logo" src={reactLogo} alt="" />
-        <h1 className='blue logo-title'>React Facts</h1>
+        <h3 className='blue logo-title'>React Facts</h3>
       </div>
 
       <p className='extra-title'>React Course - Project 1</p>
