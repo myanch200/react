953f49b297cb7639363cadfde93cac599ca2387f
Martin Yanchev <martin@icecreamarchitecture.com>
2022-09-11 20:24:56 +0100

    setting up the facts app

---
+++ react-facts/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Main from './components/Main/Main'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Main/>
+    </div>
+  )
+}
+
+export default App
