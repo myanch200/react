3fa48ce83bc2db9e6a1921d8a3cba29de3767338
Martin Yanchev <martin@icecreamarchitecture.com>
2022-09-11 20:54:57 +0100

    finishing the nav, removing not used .css files
    as I will be using only the index.css for now

--- react-facts/src/components/Navbar/Navbar.jsx
+++ react-facts/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import reactLogo from "../../assets/react.svg"
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className="nav-bar">
+      <div className="nav-logo-wrapper">
+        <img className="nav-react-logo" src={reactLogo} alt="" />
+        <h1 className='blue logo-title'>React Facts</h1>
+      </div>
+
+      <p className='extra-title'>React Course - Project 1</p>
+
+    </nav>
   )
 }
 
