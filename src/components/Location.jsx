2a9c619d4e48db1e3ba8276ff652f02e3a35439b
Martin Yanchev <martin@icecreamarchitecture.com>
2022-09-16 23:56:16 +0100

    finalizing the project
    since the project was relatively small I am creating it on one commit

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import locationIcon from '../assets/location.svg'
 
-export default function Location() {
+export default function Location(props) {
+  let item = props.item;
+
+  let credits = item.coverImg.credits
   return (
-    <div>Location</div>
+
+    <section className='location-card'>
+     <img className="location-thubnail" src={item.coverImg.src} alt="asdasd" />
+     <div className="location-card--content">
+        <div className="location-info">
+          <img  className='location-icon' src={locationIcon} alt="" />
+          <p className='location-country'>{item.location}</p>
+          <a className='link' href={item.googleMapsLink} target="_blank">View on Google Maps</a>
+        </div>
+        <h2 className='location-name'>{item.name}</h2>
+        <span className='dates'>{item.startDate} - {item.endDate}</span>
+        <p className='location-description'>{item.description}</p>
+        <div className='credit'>
+          <p>Photo taken by:</p>
+          <a className="link" target='_blank' href={credits.link}>{credits.name}</a>
+        </div>
+        
+     </div>
+    </section>
+
   )
 }
---
+++ src/models/locations.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    id: 1,
+    location: "United Kingdom",
+    name: "London",
+    startDate: "29.07.2022",
+    endDate: "01.08.2022",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare pretium nibh, ut ullamcorper nunc convallis eu. Aenean quis dolor non magna feugiat maximus. Aenean sit amet elit et ex euismod interdum. Suspendisse ultricies commodo nisi at porttitor. Aenean quis est finibus, tempus elit suscipit, aliquam orci. In molestie malesuada metus, in euismod neque tristique a. Sed posuere velit vel tempor tincidunt. Duis sodales est eu ullamcorper imperdiet.",
+    googleMapsLink: "https://goo.gl/maps/k7LAX5q6CC8Qzvvy5",
+
+    coverImg: {
+        src: "https://images.unsplash.com/photo-1520986606214-8b456906c813?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+        credits: {
+          link:"https://unsplash.com/@lucas_davies",
+          name: "Lucas Davies"
+        }
+    }
+  },
+
+  {
+    id: 2,
+    location: "Bulgaria",
+    name: "Burgas",
+    startDate: "01.03.2022",
+    endDate: "14.03.2022",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare pretium nibh, ut ullamcorper nunc convallis eu. Aenean quis dolor non magna feugiat maximus. Aenean sit amet elit et ex euismod interdum. Suspendisse ultricies commodo nisi at porttitor. Aenean quis est finibus, tempus elit suscipit, aliquam orci. In molestie malesuada metus, in euismod neque tristique a. Sed posuere velit vel tempor tincidunt. Duis sodales est eu ullamcorper imperdiet.",
+    googleMapsLink: "https://goo.gl/maps/JQfNTTE8hAE3hoca8",
+    coverImg: {
+      src: "https://images.unsplash.com/photo-1551014700-1e7146fae73b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1773&q=80",
+      credits: {
+        link: "https://unsplash.com/@ainikolov",
+        name: "Anton Nikolov"
+      }
+    }
+  }
+]
