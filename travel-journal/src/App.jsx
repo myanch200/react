1a6a97e6b714fa33fce0a148e22244f95e879d3b
Martin Yanchev <martin@icecreamarchitecture.com>
2022-09-16 23:56:16 +0100

    finalizing the project
    since the project was relatively small I am creating it on one commit

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Location from './components/Location'
+import locations from './models/locations'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let locationCards = locations.map(location => {
+    return <Location key={location.item} item={location} />
+  });
 
   return (
     <div className="App">
       <Navbar />
+      <section className='locations-list'>
+      {locationCards}
+
+      </section>
     </div>
   )
 }
