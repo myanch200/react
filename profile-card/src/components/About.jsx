49a6ee1e74948dae9006235566d0d6da87b92451
Martin Yanchev <martin@icecreamarchitecture.com>
2022-09-16 15:41:56 +0100

    final changes

--- profile-card/src/components/About.jsx
+++ profile-card/src/components/About.jsx
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 export class About extends Component {
   render() {
     return (
-      <div>About</div>
+      <section className="about-section">
+        <h2 className="about-section--title">About</h2>
+        <p className="about-section--description">
+        I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.        </p>
+      </section>
     )
   }
 }
--- profile-card/src/components/Info.jsx
+++ profile-card/src/components/Info.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from 'react'
+import profilePhoto from '../assets/images/profile-photo.jpeg'
+import emailIcon from '../assets/email.svg'
+import linkedInIcon from '../assets/linked-in.svg'
 
 export class Info extends Component {
   render() {
     return (
-      <div>Info</div>
+      <section className='info-section'>
+        <img src={profilePhoto} alt="" className="info-section--profile-photo" />
+        <h1 className="info-section--name">Martin Yanchev</h1>
+        <h3 className="info-section--position-title">Software Developer</h3>
+        <p className="info-section--website">N/A</p>
+        <div className="info-section--social-links">
+          <div className="info-section--links info-section--email">
+            <img src={emailIcon} alt="email icon" />
+            <a>Email</a>
+          </div>
+          <div className="info-section--links info-section--linkedin">
+            <img src={linkedInIcon} alt="email icon" />
+            <a>LinkedIn</a>
+          </div>
+        </div>
+      </section>
     )
   }
 }
